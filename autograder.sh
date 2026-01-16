1a97acdd779f5cdb276aa72a37acc6065f7db935
Aayush Borkar <aayushborkar@cse.iitb.ac.in>
2026-01-17 02:37:54 +0530

    autograder rc check

--- autograder.sh
+++ autograder.sh
@@ -3,9 +3,11 @@ DIR="example-programs"
 
 find "$DIR" -type f -name "*.c" | while read -r file; do
     echo "Processing $file"
+
     toks_file="${file}.toks"
     a1_toks_file="${file}.A1.toks"
     reference-implementations/A1-sclp "$file" --show-tokens
+    a1_rc=$?
 
     if [[ ! -f "$toks_file" ]]; then
         echo "ERROR: .toks file not generated for $file"
@@ -15,6 +17,10 @@ find "$DIR" -type f -name "*.c" | while read -r file; do
     mv "$toks_file" "$a1_toks_file"
 
     ./sclp --show-tokens "$file"
+    our_rc=$?
     diff -Bw "$toks_file" "$a1_toks_file"
-    # fi
+
+    if [[ $a1_rc -ne $our_rc ]]; then
+        echo "ERROR: return code mismatch for $file"
+    fi
 done
