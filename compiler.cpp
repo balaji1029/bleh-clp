b1d60c915d3317d64ad42d7c54437369bd0ae33d
Balaji <balajikaredla4@gmail.com>
2026-02-01 12:00:02 +0530

    i forgot

--- compiler.cpp
+++ compiler.cpp
@@ -5,13 +5,17 @@ Compiler::Compiler (int argc, char* argv[]) : lexer(&input_file) {
 
     static struct option long_opts[] = {
         {"show-tokens", no_argument, 0, 't'},
+        {"sa-scan", no_argument, 0, 's'},
         {0, 0, 0, 0}
     };
 
-    while ((opt = getopt_long(argc, argv, "t", long_opts, NULL)) != -1) {
+    while ((opt = getopt_long(argc, argv, "ts", long_opts, NULL)) != -1) {
         switch (opt) {
             case 't':
                 flags.show_tokens = true;
+                break;
+            case 's':
+                flags.sa_scan = true;
                 break;
             default:
                 std::cerr << ERROR << std::endl;
@@ -37,18 +41,33 @@ Compiler::Compiler (int argc, char* argv[]) : lexer(&input_file) {
     if (flags.show_tokens) output_token_file.open(output_token_filename);
 }
 
-int Compiler::parse() {
-    int status;
-
+int Compiler::run() {
+    int status = 0;
+    
     if (!input_file.is_open()) {
         std::cerr << "File not open!! :(" << std::endl;
     }
 
+    if (flags.sa_scan) return scan();
+
+    status = parse();
+    
+    if (flags.show_tokens) output_token_file << lexer.token_output << std::flush;
+
+    return status;
+}
+
+int Compiler::scan() {
+    yy::parser::semantic_type yylval;
+    while (lexer.yylex(&yylval));
+    return 0;
+}
+
+int Compiler::parse() {
+    int status = 0;
+
     yy::parser parser(lexer);
 
     status = parser.parse();
-
-    if (flags.show_tokens) output_token_file << lexer.token_output << std::flush;
-    // std::cout << lexer.token_output << std::endl;
     return status;
 }
