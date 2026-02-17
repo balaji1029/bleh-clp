72b84af2ff9503babdfbd48f9b0d07ff178b8e5f
Aayush Borkar <aayushborkar@cse.iitb.ac.in>
2026-02-17 15:09:06 +0530

    fix semantic_check

---
+++ example-programs/test.c
@@ -0,0 +1,15 @@
+void main(bool a);
+
+void main(bool d) {
+    int a;
+    int b, c;
+    // d = a > b;
+    // d = a >= b;
+    // d = a == b;
+    // d = a <= b;
+    // d = a < b;
+    // d = a != b;
+    // d = !d;
+    string e;
+    e = "";
+}
